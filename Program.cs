d1f587070171fa054277ffcd502f3dc5b0c6e470
Denisse Lemos <32619895+denulemos@users.noreply.github.com>
2017-10-12 12:26:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tecnoloshe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese Edad");
+            int edad = int.Parse(Console.ReadLine());
+            if (edad>=18)
+            {
+                Console.WriteLine("Es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Es menor, alejateeee");
+            }
+        }
+    }
+}
