fdbd4b511ede0072cf39ae3687f1320b8f54f227
Denisse Lemos <32619895+denulemos@users.noreply.github.com>
2017-10-13 20:33:31 -0300

    Cambio 2

---
+++ TecnolosheProyect-master/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tecnoloshe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese Edad");
+            int edad = int.Parse(Console.ReadLine());
+            if (edad>=18)
+            {
+                Console.WriteLine("Es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Es menor, alejateeee");
+            }
+            for (int i=0;i<6;i++)
+            {
+                Console.WriteLine("Hola!");
+            }
+            Console.WriteLine("Presione cualquier tecla para finalizar");
+            Console.ReadKey();
+            
+
+        }
+    }
+}
